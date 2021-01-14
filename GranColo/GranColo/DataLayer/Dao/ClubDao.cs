5d4d06b1a15bec4250c334d1c4571acdf554b0fa
franciscozapata12346 <53557692+franciscozapata12346@users.noreply.github.com>
2021-01-14 12:36:39 -0300

    Subida de proyecto
    Subida de proyecto

---
+++ GranColo/GranColo/DataLayer/Dao/ClubDao.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GranColo.BusinessLayer.Entities;
+
+namespace GranColo.DataLayer.Dao
+{
+    class ClubDao
+    {
+
+        internal bool InsertClub(Club oClub)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool ModifyClub(Club oClub, int selected)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool DeleteClub(int selected)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IList<Club> GetAllClub()
+        {
+            List<Club> list = new List<Club>();
+            string sql = " SELECT * FROM Club ";
+            DataTable rtados = DataManager.GetInstance().ConsultaSQL(sql);
+            foreach (DataRow row in rtados.Rows)
+            {
+                list.Add(ObjectMapping(row));
+            }
+            return list;
+        }
+
+        private Club ObjectMapping(DataRow row)
+        {
+
+            Club Club = new Club();
+            Club.Localidad = new Localidad();
+            Club.IdClub = Int32.Parse(row[0].ToString());
+            Club.Nombre = row[1].ToString();
+            Club.FechaFundacion = row[2].ToString();
+            Club.NombreEstadio = row[3].ToString();
+            Club.Aforo = Int32.Parse(row[4].ToString());
+            Club.Calle = row[6].ToString();
+            Club.Numero = Int32.Parse(row[7].ToString());
+            Club.Localidad.IdLocalidad = Int32.Parse(row[5].ToString());
+            return Club;
+        }
+    }
+}
---
+++ GranColo/GranColo/DataLayer/Dao/EstadoActualDao.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GranColo.BusinessLayer.Entities;
+
+namespace GranColo.DataLayer.Dao
+{
+    class EstadoActualDao
+    {
+        internal bool InsertEstadoActual(EstadoActual oEstadoActual)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool ModifyEstadoActual(EstadoActual oEstadoActual, int selected)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal bool DeleteEstadoActual(int selected)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal IList<EstadoActual> GetAllEstadoActual()
+        {
+            List<EstadoActual> list = new List<EstadoActual>();
+            string sql = " SELECT * FROM EstadoActual ";
+            DataTable rtados = DataManager.GetInstance().ConsultaSQL(sql);
+            foreach (DataRow row in rtados.Rows)
+            {
+                list.Add(ObjectMapping(row));
+            }
+            return list;
+        }
+
+        private EstadoActual ObjectMapping(DataRow row)
+        {
+            EstadoActual EstadoActual = new EstadoActual();
+            EstadoActual.IdEstadoActual = Int32.Parse(row[0].ToString());
+            EstadoActual.Nombre = row[1].ToString();
+            return EstadoActual;
+        }
+    }
+}
